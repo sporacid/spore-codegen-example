106a01c8cf4208f11fcd20d33c29c2a00d6d0f6b
sporacid <sporacid@gmail.com>
2024-09-27 20:57:08 -0400

    feat: Update to spore-codegen v3.0.0

--- include/codegen.hpp
+++ include/codegen.hpp
@@ -3,3 +3,14 @@
 #ifndef SPORE_CODEGEN
 #    error "cannot include codegen.hpp outside codegen"
 #endif
+
+#include "type_traits.hpp"
+
+namespace spore::codegen::example
+{
+    template <typename value_t>
+    struct type_traits<value_t>
+    {
+        static constexpr bool enabled = true;
+    };
+}
